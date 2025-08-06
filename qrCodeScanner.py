8b084a29e4faf0257453c5038e040bddec1e824f
Nisith Moragoda <nisith.nimdinu@gmail.com>
2025-08-06 23:49:01 +0530

    Create qrCodeScanner.py

---
+++ qrCodeScanner.py
@@ -0,0 +1,25 @@
+# import cv2
+# from pyzbar.pyzbar import decode
+
+# # Load image containing QR code
+# img = cv2.imread('Sample_QR_Codes/Untitled 1.png')
+
+# # Decode QR codes in the image
+# decoded_objects = decode(img)
+
+# for obj in decoded_objects:
+#     print("Type:", obj.type)
+#     print("Data:", obj.data.decode("utf-8"))
+
+#If you want to scan from webcam:
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    decoded_objects = decode(frame)
+    for obj in decoded_objects:
+        print("Data:", obj.data.decode("utf-8"))
+    cv2.imshow('QR Scanner', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
